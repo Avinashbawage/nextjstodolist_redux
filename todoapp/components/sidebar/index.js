9f2db6558a9d7f557c7f3b55e5629db6fb1952db
Avinashbawage <dan.biradar@gmail.com>
2022-11-11 18:05:19 +0530

    friday

--- todoapp/components/sidebar/index.js
+++ todoapp/components/sidebar/index.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useSelector, useDispatch } from "react-redux";
 
 const Sidebar = () => {
+  const itemlist = useSelector((state) => state.todoReducer.list);
+  console.log("This is new data", itemlist);
+
+  const mynewarray5= itemlist.filter((item)=>item.deleted == true);
+  console.log("Mynew5 array", mynewarray5)
+
+
   return (
-    <div>Sidebar</div>
+    <>
+   
+     <h3 className="notification">Notification</h3>
+     <ul>
+     {mynewarray5.map((item, index) => (
+            <li
+              style={item.Checked ? { textDecoration: "line-through" } : null}
+              key={item.id}
+            >
+              {index + 1} &nbsp;
+              
+              &nbsp;
+              {item.data} &nbsp; {console.log("All items", item)}
+             
+            </li>
+          ))}
+    
+     </ul>
+
+
+    </>
   )
 }
 
-export default Sidebar
+export default Sidebar;
